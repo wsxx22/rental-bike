b7b1780edea766adcce64f68c3039f194a3524c6
Daniel <daniel.bitka@gmail.com>
2019-05-29 19:50:36 +0200

    update

--- src/main/java/com/example/rentalbike/service/CustomUserDetailsService.java
+++ src/main/java/com/example/rentalbike/service/CustomUserDetailsService.java
@@ -23,7 +23,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new UserNotFound());
+        User user = userRepository.findByUsername(username).orElseThrow(UserNotFound::new);
 
         return new CustomUserDetails(user);
     }
